6566f16522411658ddb11cb61d0b1db562dcfaaa
Nabaraj <nabarajmagar54@gmail.com>
2025-01-15 11:06:37 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/hero/Hero"
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden">
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+    return <main>
+        <section>
+            <div className="container">
+                <div className="grid grid-cols-1 md:grid-cols-2"></div>
+            </div>
+      </section>
+  </main>
+}
+
+export default Hero
