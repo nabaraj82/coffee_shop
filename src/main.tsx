ace943b407b62778fa2d2cf2e5d0fdc8ae0f1e03
Nabaraj Magar <nabarajmagar19@gmail.com>
2026-07-08 16:37:45 +0545

    vercel insight implement

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const container = document.getElementById("root");
 
 if (container) {
   createRoot(container).render(
     <StrictMode>
+      <SpeedInsights/>
       <App />
     </StrictMode>,
   );
